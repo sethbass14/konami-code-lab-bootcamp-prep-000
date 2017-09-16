e9cd4150e47c65ef5013334120d79985c9eaa900
Seth Barden <sethbarden@gmail.com>
2017-09-16 12:54:25 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,9 +1,9 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 /*My code works in the browser. It is difficult to see what the tests are testing for in terms of..
   the structure of the function. */
-function init(e) {
+function init() {
   let index = 0
-  let e = document.body.addEventListener('keydown', function {
+  document.body.addEventListener('keydown', function(e) {
     if (parseInt(e.which || e.detail) === code[index]) {
     index ++ ;
     if (index === code.length) {
